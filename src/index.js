5b964db99c363ab39d29b4e2ed68fe3ee5344ccc
Renan <renan.fachin@hotmail.com>
2023-12-05 00:14:52 -0300

    feat: Cria validação de cpf

--- src/index.js
+++ src/index.js
@@ -19,13 +19,19 @@ app.use(express.json())
  */
 app.post('/signup', (request, response) => {
   const { cpf, name } = request.body
-  const id = uuidv4()
+
+  // Validação -> método some vai retornar true caso a condição seja satisfeita
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!"})
+  }
 
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   })
 
