00a6cda57cb910de2b35b18015709fd8f98c3682
Renan <renan.fachin@hotmail.com>
2023-12-05 18:21:23 -0300

    feat: Adicionar rota para listar extratos

--- src/index.js
+++ src/index.js
@@ -23,8 +23,8 @@ app.post('/signup', (request, response) => {
   // Validação -> método some vai retornar true caso a condição seja satisfeita
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
 
-  if(customerAlreadyExists) {
-    return response.status(400).json({ error: "Customer already exists!"})
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" })
   }
 
 
@@ -37,6 +37,16 @@ app.post('/signup', (request, response) => {
 
 
   return response.status(201).send()
+})
+
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+
+  return response.json(customer.statement)
 })
 
 
