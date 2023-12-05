e3c6c21f6c7e17aa29c415acc5da810e761b770d
Renan <renan.fachin@hotmail.com>
2023-12-05 00:04:30 -0300

    feat: Cria rota para criação de usuário

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid'
+
+const PORT = 3333
 
 const app = express()
+
+const customers = []
 
 // Habilitando a aplicação receber um json
 app.use(express.json())
 
 
-const PORT = 3333
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+app.post('/signup', (request, response) => {
+  const { cpf, name } = request.body
+  const id = uuidv4()
+
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+
+
+  return response.status(201).send()
+})
+
+
 app.listen(PORT)
